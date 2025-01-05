75dee14aa19d9333793b9df5729a03eef0f2bbcd
Tanzim Ahmed <utshotanzim@gmail.com>
2025-01-05 11:34:34 +0600

    profile added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,33 @@
+
 import React from 'react'
+import { FaDownload, FaFacebook } from 'react-icons/fa'
+import { FaInstagram, FaLinkedin } from 'react-icons/fa6'
+import { FaSquareXTwitter } from "react-icons/fa6";
+import profile from "../assets/profile.png"
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='h-max md:h-screen bg-gradient-to-l bg-gray-950 text-white flex flex-col justify-center items-center relative z-10 pb-10'>
+      <div className='max-w-7xl mt-24 mx-auto items-center flex flex-col md:flex-row gap-16 md:gap-40 justify-between'>
+        <div className='md:space-y-6 px-4'>
+          <h1 className='md:text-6xl text-4xl font-bold mb-4'>Hi 👋, I'm <p className='text-blue-400'>Tanzim Ahmed</p></h1>
+          <p className='md:text-2xl text-lg mb-3'>Frontend Developer </p>
+          <p className='lg:w-[600px] mb-4 md:mb-0 text-sm text-gray-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione optio ipsa perspiciatis voluptatibus ducimus fugit. Eos consequatur expedita cumque accusantium consequuntur veniam necessitatibus enim nemo!</p>
+         <button className='px-3 py-2 flex items-center gap-2 bg-blue-500 rounded-md text-white hover:bg-blue-600 shadow-blue-500 border-2 border-blue-400 transition-all shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#33CCCC,0_0_15px_#33CCCC,0_0_30px_#33CCCC]'> <FaDownload/> Download CV</button>
+         <div className='flex gap-3 text-2xl transition-all mt-5 md:mt-0'>
+          <FaFacebook className='hover:text-blue-400'/>
+          <FaInstagram className='hover:text-blue-400'/>
+          <FaLinkedin className='hover:text-blue-400'/>
+          <FaSquareXTwitter className='hover:text-blue-400'/>
+         </div>
+        </div>
+        <div className='relative group'>
+          <img src={profile}  alt="" className='rounded-full border border-blue-600 md:w-[500px] w-[300px] shadow-[0px_0px_20px_10px-rgb(0,0,0,0.3)] shadow-blue-500' />
+        </div>
+      </div>
+    </section>
   )
 }
 
 export default Hero
+
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Logo from '../assets/Logo.png'
+import { FaChevronRight } from "react-icons/fa";
+import { HiMenuAlt3,HiMenuAlt1  } from "react-icons/hi";
+import ResponsiveMenu from './ResponsiveMenu';
+
 
 const Navbar = () => {
+    const[showMenu, setShowMenu]=useState(false)
+    const toggleMenu = () => {
+        setShowMenu(!showMenu)
+    }
   return (
-    <div>Navbar</div>
+    <div className='bg-gray-950 -mb-7 z-50 w-full py-3 fixed'>
+        <div className='flex items-center justify-between max-w-7xl mx-auto px-4 md:px-0'>
+            {/* Logo Section */}
+<div>
+    <a href="#"><img src={Logo} alt="Logo" className='w-[170px]' /></a>
+</div>
+{/* Menu Section */}
+<nav className='hidden md:block'>
+    <ul className='flex gap-7 text-xl items-center font-semibold text-white'>
+        <a href="#"><li className='cursor-pointer'>Home</li></a>
+        <a href="#about"><li className='cursor-pointer'>About</li></a>
+        <a href="#education"><li className='cursor-pointer'>Education & Experience</li></a>
+        <a href="#project"><li className='cursor-pointer'>Projects</li></a>
+        <a href="#contact"><button className='px-3 py-1 cursor-pointer rounded-md bg-blue-500 text-white flex items-center gap-1 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0__0_5px_#33CCCC,0_0_15PX_#33CCCC,0_0_30px_#33CCCC] shadow-blue-500 border-blue-400 border-2'>Contact <FaChevronRight/></button></a>
+    </ul>
+
+</nav>
+<div className='md:hidden text-white text-4xl'>
+{
+    showMenu ? <HiMenuAlt3 onClick={toggleMenu}/> : <HiMenuAlt1 onClick={toggleMenu}/>
+}
+</div>
+        </div>
+        <ResponsiveMenu showMenu={showMenu} setShowMenu={setShowMenu}/>
+    </div>
   )
 }
 
